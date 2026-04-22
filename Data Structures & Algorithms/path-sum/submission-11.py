5be2e396e38b12197e092a96dbf413f252df040e
Chi Yang <chiyang.lucas@gmail.com>
2026-04-22 08:21:24 -0500

    Add: path-sum - submission-11

---
+++ Data Structures & Algorithms/path-sum/submission-11.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+
+        def dfs(node, curr):
+            if not node:
+                return False
+            
+            curr += node.val
+            if not node.left and not node.right:
+                return curr == targetSum
+            
+            return dfs(node.left, curr) or dfs(node.right, curr)
+
+        return dfs(root, 0)
