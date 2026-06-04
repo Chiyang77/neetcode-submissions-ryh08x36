d7e80e3ce89e999a10abe363a26f122358bd7bea
Chi Yang <chiyang.lucas@gmail.com>
2026-06-04 17:53:05 -0500

    Add: minimum-path-sum - submission-5

---
+++ Data Structures & Algorithms/minimum-path-sum/submission-5.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        nrow = len(grid)
+        ncol = len(grid[0])
+        memo={}
+
+        def dfs(i,j):
+            if i == nrow or j==ncol:
+                return float('inf')
+            if i==nrow-1 and j==ncol-1:
+                return grid[i][j]
+            if(i,j) in memo:
+                return memo[(i,j)]
+
+            memo[(i,j)] = grid[i][j]+min(dfs(i+1,j), dfs(i,j+1))
+            return memo[(i,j)]
+        return dfs(0,0)
